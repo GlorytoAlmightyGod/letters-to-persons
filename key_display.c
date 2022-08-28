9dccf09c2617d4e897c24fe414e03bc8445bfed9
Dasarath Sankar Sai Kamarajugadda <dakamara@cisco.com>
2022-08-28 15:08:56 +0530

    corrected some print statement

--- key_display.c
+++ key_display.c
@@ -5,12 +5,12 @@ void display_CODE_of_the_letter(int number_on_letter){
     printf("-------------LETTER %d-------------", number_on_letter);
     printf("\n");
     for(alphabet_index = 1 ; alphabet_index <= 26 ; alphabet_index++){
-        printf("%c in encrypted_letter is %c in RAW-LETTER.", convert_to_alphabet(alphabet_index, true), convert_to_alphabet(decrypt_the_alphabet(alphabet_index, number_on_letter), true));
+        printf("%c in the encryption is %c in Reality.", convert_to_alphabet(alphabet_index, true), convert_to_alphabet(decrypt_the_alphabet(alphabet_index, number_on_letter), true));
         printf("\n");
     }
     printf("\n");
     for(alphabet_index = 1 ; alphabet_index <= 26 ; alphabet_index++){
-        printf("%c in RAW-LETTER is to be %c on encrypted_letter.", convert_to_alphabet(alphabet_index, true), convert_to_alphabet(encrypt_the_alphabet(alphabet_index, number_on_letter), true));
+        printf("%c in Reality is to be %c on the Encryption.", convert_to_alphabet(alphabet_index, true), convert_to_alphabet(encrypt_the_alphabet(alphabet_index, number_on_letter), true));
         printf("\n");
     }
     printf("\n");
